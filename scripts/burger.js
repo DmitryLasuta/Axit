92c6995c0c04b84dc22861a27bc5157dc6963b9c
Дмитрий Ласута <dmitriy.lasuta@uptrade.ru>
2024-11-24 02:27:08 +0300

    refactor: prepareed project

--- scripts/burger.js
+++
@@ -1,6 +0,0 @@
-window.onload = () => {
-  let burgerBtn = document.querySelector(".header__menu-burger");
-  burgerBtn.addEventListener("click", () => {
-    burgerBtn.classList.toggle("header__menu-burger--active");
-  });
-};
